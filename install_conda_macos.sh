eb30d37f7f228ad7f6d32c11cf12b52ebe56451e
dougpagani <redstonefreedom@gmail.com>
2018-02-18 02:39:00 -0500

    blah

--- install_conda_macos.sh
+++ install_conda_macos.sh
@@ -1,19 +1,30 @@
 # install_conda_macos.sh
 ################################################################################
 
+#CONDA_DEST="$HOME/miniconda"
+CONDA_DEST="/usr/local/miniconda"
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
-bash ~/miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-echo 'export PATH="$HOME/miniconda/bin:$PATH"' >>~/.bashrc
-echo 'export PATH="$HOME/miniconda/bin:$PATH"' >>~/.bash_profile
-echo 'source activate zipper' >> ~/.bash_profile
+bash ~/miniconda.sh -b -p "$CONDA_DEST"
+
+CONDA_BIN="$CONDA_DEST/bin"
+export PATH="/usr/local/miniconda/bin:$PATH"
+echo "export PATH=${CONDA_BIN}:"'$PATH' >>~/.bashrc
+echo "export PATH=${CONDA_BIN}:"'$PATH' >>~/.bash_profile
+
+#echo 'source activate zipper' >> ~/.bash_profile
+echo "
+alias zipper='source activate zipper'
+    alias zipup='source activate zipper'
+    alias zipp='source activate zipper'
+" >> ~/.bash_profile
 
 conda update conda --yes
 
-conda-env remove -n zipper --yes 2> /dev/null
-conda env create -n zipper -f env.yml
+conda env remove -n zipper --yes 2> /dev/null
+conda env create -n zipper -f ./env.yml || 
+    echo "make sure you're in the arturo directory"
 # env.yml generated with:
 #   $ source activate zipper && conda env export > ./env.yml 
 
-# Activate it & get rolling
-source activate zipper
+# Activate it & get rolling -- only works if being sourced
+#source activate zipper
