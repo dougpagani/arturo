fc7c07e88aea007bda12f9a2e3d654eb7e0f191d
DP@arturo <redstonefreedom@gmail.com>
2018-02-25 20:42:53 +0000

    changed start/end_session to more feasible values

--- install/extension.py
+++ install/extension.py
@@ -1,3 +1,5 @@
+# This script INGESTS the csv's in the directory "minute" 
+# ... found at /csv_data , bound to the docker-image
 import pandas as pd
 
 from zipline.data.bundles import register
@@ -5,8 +7,11 @@ from zipline.data.bundles.csvdir import csvdir_equities
 
 CSVDIR = '/csv_data'
 
-start_session = pd.Timestamp('2012-7-10', tz='utc')
-end_session = pd.Timestamp('2017-05-15', tz='utc')
+# Will ingest with this range
+# TODO: make a main() which accepts args to digest as desired
+# TODO: will have to move the csv's, or parameterize convert_csv.py, to match
+start_session = pd.Timestamp('2018-01-02', tz='utc')
+end_session = pd.Timestamp('2017-02-01', tz='utc')
 
 register(
     'csv-bundle',
