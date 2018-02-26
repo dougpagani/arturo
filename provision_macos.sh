808b6c6323d71c0c0479b04ccc45e523862c9bf2
dougpagani <redstonefreedom@gmail.com>
2018-02-26 15:19:46 -0500

    master script for macos, for provisioning a developer's env

---
+++ provision_macos.sh
@@ -0,0 +1,8 @@
+# provision_macos.sh
+#    For all those pesky pieces of zipline + whatever.
+#       1. docker of quantopian-fork
+#       2. docker of my fork
+#       3. conda installed locally, with pip install -e
+#       TODO: convert install_conda ... to a manual, non-env.yml-dependent build
+################################################################################
+#
